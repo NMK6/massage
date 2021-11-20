fbb048077d68ca9bfd1848e2bed6e02b6ad72c9c
NMK6 <mkehre@gmail.com>
2021-11-20 23:46:22 +0000

    moved video to js

--- src/js/views/homeView.js
+++ src/js/views/homeView.js
@@ -1,20 +1,40 @@
-// import { elements } from './base';
+import { elements } from './base';
 // import * as utils from './utils';
 export function addMarkup(arr) {
-  const yearContainer = document.querySelector('.footer__year');
-  yearContainer.textContent = arr.year;
+	const yearContainer = document.querySelector('.footer__year');
+	const backgroundVideo = document.querySelector('.background');
+	backgroundVideo.style.height = elements.rootWidth / 1.82 + 'px';
+	yearContainer.textContent = arr.year;
 
-  //   const container = utils.createNewElement('div', 'home', elements.root);
-  //   const firstHalf = utils.createNewElement(
-  //     'section',
-  //     'home__half main about contact',
-  //     container
-  //   );
-  //   const secondHalf = utils.createNewElement(
-  //     'section',
-  //     'home__half map',
-  //     container
-  //   );
-  //   const containers = [firstHalf, secondHalf];
-  //   return containers;
+	setTimeout(function () {
+		const videoElement = `<video
+    playsinline
+    autoplay
+    muted
+    loop
+    poster="/img/cover.jpg"
+    class="background__video"
+  >
+    <source src="/video/background.mp4" type="video/mp4" />
+  </video>`;
+		const videoContainer = document.querySelector(
+			'.background__video-container'
+		);
+		videoContainer.insertAdjacentHTML('beforeend', videoElement);
+		videoContainer.classList.add('add-opacity');
+	}, 100);
+
+	//   const container = utils.createNewElement('div', 'home', elements.root);
+	//   const firstHalf = utils.createNewElement(
+	//     'section',
+	//     'home__half main about contact',
+	//     container
+	//   );
+	//   const secondHalf = utils.createNewElement(
+	//     'section',
+	//     'home__half map',
+	//     container
+	//   );
+	//   const containers = [firstHalf, secondHalf];
+	//   return containers;
 }
